ada07e548928b1519d01ec032656fa0fe48d47e6
rizaghoshmitra <20111569@tafe.wa.edu.au>
2024-06-07 10:36:11 +0800

    feat(project): Version Control Setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html',
+  './about.html',
+  './resume.html',
+  './projects.html',
+  './mywork.html',
+  './contact.html',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
