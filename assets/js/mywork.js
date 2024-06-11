d7d2f40b2f2c1f8e74704f02d508b0da911b5c53
rizaghoshmitra <20111569@tafe.wa.edu.au>
2024-06-11 10:48:17 +0800

    fix:correction final project

--- assets/js/mywork.js
+++ assets/js/mywork.js
@@ -1,4 +1,10 @@
 document.getElementById('menu-toggle').addEventListener('click', function () {
-    var menu = document.getElementById('mobile-menu');
+    var menu = document.getElementById('mobile-menuu');
     menu.classList.toggle('hidden');
+});
+window.addEventListener('resize', function () {
+    var menu = document.getElementById('mobile-menuu');
+    if (window.innerWidth > 768) {
+        menu.classList.add('hidden');
+    }
 });
